0c4b68f67e7e92d8be22f888ecb8c71def2c2deb
Said Bouhmouch <said.dev.info@gmail.com>
2022-10-28 19:20:22 +0100

    set default bridge network

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const app = express();
 const port = 4000;
 
 
-app.get('/', (req, resp) => resp.send('<h1> Hello World. </h1>'));
+app.get('/', (req, resp) => resp.send('<h1> Hello World . </h1>'));
 
 app.listen(port, () => console.log('app is up and running ...'));
